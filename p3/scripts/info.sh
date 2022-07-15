2c2a99f8ac7cd49d46a2141d6f5a38d1d5d52bfa
Dmytro Shults <dishults@icloud.com>
2022-07-15 17:44:12 +0200

    Rename info file

--- p3/scripts/info.sh
+++ p3/scripts/info.sh
@@ -4,7 +4,7 @@ GREEN="\033[32m"
 NC="\033[0m" # No Color
 
 ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
-echo "${GREEN}Argo CD API server is accessible at http://localhost:8080 ${NC}"
+echo "${GREEN}Argo CD: http://localhost:8080 ${NC}"
 echo "username: admin, password: ${ARGOCD_PASSWORD}"
 
-echo "\n${GREEN}wil-playground app is accessible at http://localhost:8888 ${NC}"
+echo "\n${GREEN}wil-playground: http://localhost:8888 ${NC}"
