21277bb0b12192fcaf38c38c6f5571f15b2feac9
Dmytro Shults <dishults@icloud.com>
2022-07-15 15:05:24 +0200

    Expose services with NodePort and K3d agents

--- p3/scripts/configure.sh
+++ p3/scripts/configure.sh
@@ -4,7 +4,7 @@ GREEN="\033[32m"
 NC="\033[0m" # No Color
 
 echo "${GREEN}Creating a cluster${NC}"
-k3d cluster create inception-of-things --wait
+k3d cluster create inception-of-things --agents 2 -p 8080:30080@agent:0 -p 8888:30088@agent:1 --wait
 
 echo "\n${GREEN}Creating two namespaces${NC}"
 kubectl create namespace argocd
@@ -14,17 +14,14 @@ echo "\n${GREEN}Setting current context default namespace to Argo CD${NC}"
 kubectl config set-context --current --namespace=argocd
 
 echo "\n${GREEN}Installing Argo CD${NC}"
-kubectl apply -f ../confs/install.yaml --wait
+kubectl apply -f ../confs/argo_cd.yaml --wait
 
 echo "\n${GREEN}Waiting for Argo CD pods to finish initializing${NC}"
 for pod in $(kubectl get deploy -o name); do kubectl rollout status $pod; done
 
 echo "\n${GREEN}Logging in to Argo CD${NC}"
-NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
-NODE_PORT=$(kubectl get services argocd-server -o jsonpath="{.spec.ports[0].nodePort}")
-ARGOCD_SERVER=$NODE_IP:$NODE_PORT
 ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
-argocd login $ARGOCD_SERVER --plaintext --insecure --username admin --password $ARGOCD_PASSWORD
+argocd login localhost:8080 --plaintext --insecure --username admin --password $ARGOCD_PASSWORD
 
 echo "\n${GREEN}Creating wil-playground app${NC}"
-kubectl apply -f ../confs/application.yaml --wait
+kubectl apply -f ../confs/wil_playground.yaml --wait
--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -3,13 +3,8 @@
 GREEN="\033[32m"
 NC="\033[0m" # No Color
 
-NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
-NODE_PORT=$(kubectl get services argocd-server -o jsonpath="{.spec.ports[0].nodePort}")
-ARGOCD_SERVER=$NODE_IP:$NODE_PORT
 ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
-
-echo "${GREEN}Argo CD API server is accessible at http://${ARGOCD_SERVER} ${NC}"
+echo "${GREEN}Argo CD API server is accessible at http://localhost:8080 ${NC}"
 echo "username: admin, password: ${ARGOCD_PASSWORD}"
 
-echo "\n${GREEN}Starting port forwarding for wil-playground app, it's accessible at: http://localhost:8888 ${NC}"
-kubectl port-forward svc/wil-playground -n dev 8888:8888
+echo "\n${GREEN}wil-playground app is accessible at http://localhost:8888 ${NC}"
