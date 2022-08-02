370886eec7fba40849fb3109d9c1f01acc10706e
ebenaiss <elwadie.benaissa@gmail.com>
2022-08-02 09:46:46 +0200

    removes the use of shared folder

--- p1/scripts/dshultsSW.sh
+++ p1/scripts/dshultsSW.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /vagrant/scripts/node-token --node-ip=$2"
+## récupération du token k3s du master
+mkdir -p /root/.ssh
+mv /tmp/id_rsa /root/.ssh
+scp -o StrictHostKeyChecking=no root@192.168.42.110:/var/lib/rancher/k3s/server/token /tmp/token
+
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /tmp/token --node-ip=$2"
 curl -sfL https://get.k3s.io | sh -
 
-sudo yum install net-tools -y
+# sudo yum install net-tools -y
 
 ## pour désinstaller
 # /usr/local/bin/k3s-uninstall.sh
--- p1/scripts/ebenaissS.sh
+++ p1/scripts/ebenaissS.sh
@@ -5,10 +5,12 @@ export INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1 --bind-
 curl -sfL https://get.k3s.io |  sh -
 
 ## pour rendre disponible le token aux agents
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/
+#sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/
+mkdir -p /root/.ssh
+cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
 
 ## pour rendre disponible la commande 'ifconfig' durant la correction, la commande 'ip' présente de base aurait suffit...
-sudo yum install net-tools -y
+#sudo yum install net-tools -y
 
 ## pour désinstaller
 # /usr/local/bin/k3s-uninstall.sh
