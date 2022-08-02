685741f2f2bd800d8dc9f0f1161db13fdef602c6
ebenaiss <elwadie.benaissa@gmail.com>
2022-08-02 14:21:12 +0200

    makes the p1 work faster but not yet tested on a vm

--- p1/scripts/dshultsSW.sh
+++ p1/scripts/dshultsSW.sh
@@ -3,6 +3,7 @@
 ## récupération du token k3s du master
 mkdir -p /root/.ssh
 mv /tmp/id_rsa /root/.ssh
+sudo su vagrant
 scp -o StrictHostKeyChecking=no root@192.168.42.110:/var/lib/rancher/k3s/server/token /tmp/token
 
 export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /tmp/token --node-ip=$2"
