75b581a7b6f48e716cdf0acb8fe646dba673acdb
ebenaiss <elwadie.benaissa@gmail.com>
2022-08-04 16:03:59 +0200

    normally contains a working p2, to be tested on a virtual machine

--- p2/scripts/ebenaissS.sh
+++ p2/scripts/ebenaissS.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1"
 curl -sfL https://get.k3s.io |  sh -
 
-sudo yum install net-tools -y
+/usr/local/bin/kubectl -n kube-system apply -f /vagrant/confs
+
+#pour voir l'ingress on utilise kubectl en précisant le namespace qui va bien : kubectl get ingress -n kube-system
+#sinon : kubectl get all -n kube-system permet de voir le reste
